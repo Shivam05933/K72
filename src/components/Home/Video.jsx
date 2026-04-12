e387e6e2f8abaf0303e79ba4de6d0f3dbda00b90
Shivam Vishwakarma <shivamv865593@gmail.com>
2026-04-12 13:38:27 +0530

    video url added

--- src/components/Home/Video.jsx
+++ src/components/Home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='w-full h-full'>
-      <video className='h-full w-full object-cover' autoPlay loop muted src="../public/video.mp4"></video>
+      <video className='h-full w-full object-cover' autoPlay loop muted src="https://res.cloudinary.com/davthjkiw/video/upload/video_vhfdgr.mp4"></video>
     </div>
   )
 }
